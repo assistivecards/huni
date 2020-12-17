40bd64238a1d6089cdb10a6fe0373d141d0273b2
Burak Tokak <iletisim@buraktokak.com>
2020-12-17 06:45:06 +0300

    testable mvp demo

--- components/WordItem.js
+++ components/WordItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Platform, Text, View, TouchableOpacity, StatusBar, Dimensions, Image, TextInput, Animated } from 'react-native';
+import { StyleSheet, Platform, Text, View, TouchableOpacity, StatusBar, Dimensions, Image, TextInput, Animated, TouchableWithoutFeedback } from 'react-native';
 import { Image as CachedImage } from "react-native-expo-image-cache";
+import Svg, { Path } from 'react-native-svg';
 
 import API from '../api'
 import TouchableScale from 'touchable-scale-btk'
@@ -8,7 +9,8 @@ import TouchableScale from 'touchable-scale-btk'
 export default class App extends React.Component {
 
   state = {
-    colorAnim: new Animated.Value(this.props.active ? 1 : 0)
+    colorAnim: new Animated.Value(this.props.active ? 1 : 0),
+    disableButton: false
   }
 
   speak(text, speed){
@@ -29,6 +31,15 @@ export default class App extends React.Component {
     }
   }
 
+  async pressed(){
+    this.setState({disableButton: true});
+    await this.props.listen();
+
+    setTimeout(() => {
+      this.setState({disableButton: false});
+    }, 1200);
+  }
+
   render(){
     var color = this.state.colorAnim.interpolate({
         inputRange: [0, 1],
@@ -41,6 +52,20 @@ export default class App extends React.Component {
         <Animated.View style={[styles.item, {flexDirection: API.user.isRTL ? "row-reverse" : "row", backgroundColor: result.type == 1 ? result.color : "#F7F7F7", borderWidth: 2, borderColor: color}]}>
           <CachedImage uri={`${API.assetEndpoint}cards/${result.pack}/${result.slug}.png?v=${API.version}`} style={{width: API.isTablet ? 70 : 50, height: API.isTablet ? 70 : 50, margin: 5}}/>
           <Text style={[styles.searchItemText, {fontSize: result.type == 2 ? 16 : 19, marginLeft: result.type == 2 ? 0 : 10}]}>{result.title}</Text>
+          {(this.props.active && !this.state.disableButton) &&
+            <TouchableOpacity onPress={() => this.pressed()}>
+              <Animated.View style={{paddingHorizontal: 12, height: 38, backgroundColor: "#92c9cc", opacity: this.state.colorAnim, flexDirection: "row", justifyContent: "center", alignItems: "center", borderRadius: 20}}>
+                <Svg width={26} height={26} viewBox="0 0 24 24" strokeLinecap="round" strokeWidth="2" stroke="#000" fill="none" style={{marginRight: 5, opacity: 0.7}}>
+                  <Path stroke="none" d="M0 0h24v24H0z"/>
+                  <Path d="M0 0h24v24H0z" stroke="none"/>
+                  <Path d="M15 8a5 5 0 0 1 0 8"/>
+                  <Path d="M17.7 5a9 9 0 0 1 0 14"/>
+                  <Path d="M6 15 h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5"/>
+                </Svg>
+                <Text style={{color: "#000", fontWeight: "bold", fontSize: 16, opacity: 0.55}}>{API.t("training_button_listen")}</Text>
+              </Animated.View>
+            </TouchableOpacity>
+          }
         </Animated.View>
       </View>
     );
